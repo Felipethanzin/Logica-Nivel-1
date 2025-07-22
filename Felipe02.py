76d8dd44549188b1c9a66cad2e8c61dfc1f1d148
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-07-22 09:53:59 -0300

    Felipe02.py

---
+++ Felipe02.py
@@ -0,0 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Felipe Miguel")
+print("Rio de Janeiro")
+print("Nova Iguaçu")
