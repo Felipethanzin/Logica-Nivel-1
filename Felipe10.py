426e9f3c9441c035502c0977811ccbf90f86802c
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-07-22 15:39:17 -0300

    Felipe10.py

---
+++ Felipe10.py
@@ -0,0 +1,13 @@
+#10 -Escreva um software informe a média aritmética de um aluno que cursou 4 bimestres
+
+
+print("ADCIONE AS NOTAS DE CADA BIMESTRE ABAIXO")
+
+Nota1=int(input("1 BIMESTRE:"))
+Nota2=int(input("2 BIMESTRE:"))
+Nota3=int(input("3 BIMESTRE:"))
+Nota4=int(input("4 BIMESTRE:"))
+
+media=(Nota1+Nota2+Nota3+Nota4) /4
+
+print("SUA MEDIA E:",media)
