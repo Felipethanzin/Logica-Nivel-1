7abd0832211992cd422e1014ac855cc5390690e0
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-07-24 10:44:31 -0300

    Felipe12.py

---
+++ Felipe12.py
@@ -0,0 +1,9 @@
+#12 - Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+
+
+Base =int(input("Qual a base do retangulo:"))
+Altura=int(input("Qual a altura do retangulo:"))
+
+area= Base * Altura
+
+print("A area do retangulo e:",area)
