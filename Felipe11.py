cee3a85ace4c2b29e5aeb1fa6ae0c2f09f1075ef
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-07-24 10:39:55 -0300

    Felipe11.py

---
+++ Felipe11.py
@@ -0,0 +1,7 @@
+#11 - Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+numero=int(input("Digite um numero:"))
+
+antecessor =numero -1
+
+print("o antecessor é:",antecessor)
