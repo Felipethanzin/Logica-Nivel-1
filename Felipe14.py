96e6c377c07980339840d0af52642343f762305a
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-08-06 17:31:04 -0300

    Felipe14.py

---
+++ Felipe14.py
@@ -0,0 +1,10 @@
+#14) Ler um valor e escrever a mensagem É MAIOR QUE 10! Se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+numero= int(input("Digite um numero:"))
+
+if numero > 10:
+    
+     print("Esse numero é maior que o 10")
+
+else:
+     print("Esse numero não é maior que o 10")
