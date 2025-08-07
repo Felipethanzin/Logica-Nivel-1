46b311e1b8b7f5f0db5719223ece34c722c29895
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-08-07 19:18:38 -0300

    Felipe15.py

---
+++ Felipe15.py
@@ -0,0 +1,9 @@
+#15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+
+valor = float(input("Digite um valor: "))
+
+
+if valor >= 0:
+    print("O valor é positivo.")
+else:
+    print("O valor é negativo.")
